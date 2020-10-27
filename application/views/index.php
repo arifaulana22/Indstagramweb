5f67c1c79e06cbbe8d4c54e78fdb18793e4263cc
arifmaulana22 <aropon.am@gmail.com>
2020-10-27 11:48:10 +0700

    versi 1.0.0

---
+++ application/views/index.php
@@ -0,0 +1,24 @@
+<div class="container" style="margin-top:30px">
+  <div class="row">
+
+    <div class="col-md-6 offset-md-3">
+				<br>
+      <div class="img">
+        <p> Diupload oleh: afiqrasyidm@gmail.com</p> </p>
+				<a href="<?php echo site_url('gambar/detail/1'); ?>"> <img src="https://appharbor.com/assets/images/stackoverflow-logo.png" alt="..." class="img-thumbnail"> </a>
+			</div>
+			<br>
+    	<hr style="height:1px; border:none; color:#000; background-color:#000;">
+
+			<br>
+      <div class="img">
+        <p> Diupload oleh: afiqrasyidm</p> </p>
+        <a href="<?php echo site_url('gambar/detail/1'); ?>"> <img src="https://appharbor.com/assets/images/stackoverflow-logo.png" alt="..." class="img-thumbnail"> </a>
+      </div>
+      <br>
+      <hr style="height:1px; border:none; color:#000; background-color:#000;">
+
+      <br>
+    </div>
+  </div>
+</div>
